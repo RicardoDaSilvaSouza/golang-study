ecdea950b7fab53d4bc5e2b488e3d0a2f0d5b221
Ricardo da Silva Souza <ricardo.souza@c6bank.com>
2019-03-05 17:01:15 -0300

    Add \n in the end of Printf's string

--- evenodd/main.go
+++ evenodd/main.go
@@ -6,9 +6,9 @@ func main() {
 	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	for _, number := range numbers {
 		if number%2 == 0 {
-			fmt.Printf("%d is even", number)
+			fmt.Printf("%d is even\n", number)
 		} else {
-			fmt.Printf("%d is odd", number)
+			fmt.Printf("%d is odd\n", number)
 		}
 	}
 }
